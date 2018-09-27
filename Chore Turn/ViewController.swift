51abcf47e02ae007792c3e9f9c28d431c95b7d31
michaelhyun <michael.hyun3@gmail.com>
2018-09-26 20:30:10 -0700

    Initial Commit

---
+++ Chore Turn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chore Turn
+//
+//  Created by Michael Hyun on 9/26/18.
+//  Copyright © 2018 Michael Hyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
